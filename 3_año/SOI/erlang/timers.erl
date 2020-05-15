d954c3e7865170d6c84eface3ae9f14b39b884cc
natimellino <natimellino.nb@gmail.com>
2020-05-15 16:03:54 -0300

    guardo progreso

--- 3_año/SOI/erlang/timers.erl
+++ 3_año/SOI/erlang/timers.erl
@@ -1,6 +1,12 @@
 -module(timers).
--import(timer, [sleep/1]).
--export([wait/1]).
+-import(timer, [sleep/1, exit_after/2, apply_interval/4]).
+-export([wait/1, cronometro/3, f/0]).
 
 wait(Time) -> sleep(Time).
+
+f() -> io:format("Tick ~n").
+
+cronometro(F, Time, Per)->
+  exit_after(Time, self()),
+  apply_interval(Per, ti, F, []).
 
